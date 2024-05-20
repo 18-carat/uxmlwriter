d84d4bd984212c3e6d3fdfdfb922da574e990dcd
Madeleine <152932131+18-carat@users.noreply.github.com>
2024-05-20 12:00:24 -0500

    0.2.0
    Fix compatibility with old xmlwriter API

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,43 @@
 mod status;
+mod tests;
 
 use crate::status::Status;
 use std::fmt::Display;
-use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{BufWriter, Cursor, Write};
 
-pub struct XmlWriter {
-    file: BufWriter<File>,
+pub struct XmlWriter<T: Write> {
+    writer: BufWriter<T>,
     status: Status,
     tags: Vec<String>,
 }
 
-impl XmlWriter {
-    pub fn new(file: File) -> Self {
+impl Default for XmlWriter<Cursor<Vec<u8>>> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl XmlWriter<Cursor<Vec<u8>>> {
+    pub fn new() -> Self {
+        let cursor = Cursor::new(Vec::new());
         Self {
-            file: BufWriter::new(file),
+            writer: BufWriter::new(cursor),
+            status: Status::Closed,
+            tags: Vec::new(),
+        }
+    }
+
+    pub fn end_document(&mut self) -> String {
+        self.end_document_no_return();
+        let buffer = self.writer.get_ref().get_ref().clone();
+        String::from_utf8(buffer).unwrap()
+    }
+}
+
+impl<T: Write> XmlWriter<T> {
+    pub fn with_writer(writer: T) -> Self {
+        Self {
+            writer: BufWriter::new(writer),
             status: Status::Closed,
             tags: Vec::new(),
         }
@@ -49,11 +72,11 @@ impl XmlWriter {
         }
     }
 
-    pub fn write_attribute<T: Display>(&mut self, key: &str, value: T) {
+    pub fn write_attribute<U: Display>(&mut self, key: &str, value: U) {
         self.write(format!(" {}=\"{}\"", key, value).as_str());
     }
 
-    pub fn write_text<T: Display>(&mut self, text: T) {
+    pub fn write_text<U: Display>(&mut self, text: U) {
         if self.status != Status::Inside {
             self.write(">");
             self.status = Status::Inside;
@@ -71,21 +94,21 @@ impl XmlWriter {
         self.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
     }
 
-    pub fn end_document(&mut self) {
+    pub fn end_document_no_return(&mut self) {
         while !self.tags.is_empty() {
             self.close_element();
         }
 
-        match self.file.flush() {
+        match self.writer.flush() {
             Ok(_) => (),
-            Err(e) => eprintln!("Error flushing file: {}", e),
+            Err(e) => eprintln!("Error flushing writer: {}", e),
         }
     }
 
     fn write(&mut self, s: &str) {
-        match self.file.write_all(s.as_bytes()) {
+        match self.writer.write_all(s.as_bytes()) {
             Ok(_) => (),
-            Err(e) => eprintln!("Error writing to file: {}", e),
+            Err(e) => eprintln!("Error writing to writer: {}", e),
         }
     }
 
@@ -93,65 +116,4 @@ impl XmlWriter {
         self.write("\n");
         (0..self.tags.len()).for_each(|_| self.write("    "));
     }
-}
-
-#[test]
-fn maria() {
-    let file = File::create("test.xml").unwrap();
-    let mut w = XmlWriter::new(file);
-    let expected = r#"<?xml version="1.0" encoding="UTF-8"?>
-<compendium xmlns:exsl="http://exslt.org/common" version="5" auto_indent="NO">
-    <item>
-        <name>Maria</name>
-        <text/>
-        <!-- You've made your last delivery kid. -->
-        <!-- Sorry you got twisted up in this scene. -->
-        <!-- From where you're kneeling it must seem like an 18-carat run of bad luck. -->
-        <!-- Truth is... -->
-        <!-- The game was rigged from the start. -->
-        <type>Pistol</type>
-        <damage>20</damage>
-        <rate>3.8</rate>
-        <weight>1.5</weight>
-        <value>999</value>
-        <owner>Benny</owner>
-    </item>
-</compendium>"#;
-
-    w.write_declaration();
-    w.start_element("compendium");
-    w.write_attribute("xmlns:exsl", "http://exslt.org/common");
-    w.write_attribute("version", 5);
-    w.write_attribute("auto_indent", "NO");
-    w.start_element("item");
-    w.start_element("name");
-    w.write_text("Maria");
-    w.close_element();
-    w.start_element("text");
-    w.close_element();
-    w.write_comment("You've made your last delivery kid.");
-    w.write_comment("Sorry you got twisted up in this scene.");
-    w.write_comment("From where you're kneeling it must seem like an 18-carat run of bad luck.");
-    w.write_comment("Truth is...");
-    w.write_comment("The game was rigged from the start.");
-    w.start_element("type");
-    w.write_text("Pistol");
-    w.close_element();
-    w.start_element("damage");
-    w.write_text("20");
-    w.close_element();
-    w.start_element("rate");
-    w.write_text(3.8);
-    w.close_element();
-    w.start_element("weight");
-    w.write_text(1.5);
-    w.close_element();
-    w.start_element("value");
-    w.write_text(999);
-    w.close_element();
-    w.start_element("owner");
-    w.write_text("Benny");
-    w.end_document();
-
-    assert_eq!(std::fs::read_to_string("test.xml").unwrap(), expected);
 }
